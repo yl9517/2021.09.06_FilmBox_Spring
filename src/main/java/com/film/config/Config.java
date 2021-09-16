67c833720b31d81b30e235d3e7e4b5011aea7775
wonhyoung <ybgd18@gmail.com>
2021-09-16 17:55:32 +0900

    push

--- src/main/java/com/film/config/Config.java
+++ src/main/java/com/film/config/Config.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages = {"com.kosta.mapper"})
-@ComponentScan(basePackages = {"com.kosta.service"})
+@MapperScan(basePackages = {"com.film.mapper"})
+@ComponentScan(basePackages = {"com.film.service"})
 public class Config {
 	
 }
