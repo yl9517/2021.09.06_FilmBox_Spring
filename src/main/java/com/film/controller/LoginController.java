743b353a3551dedad12b6ceb23e2d689a8b9d143
seora <seorachoi08@gmail.com>
2021-09-30 09:39:29 +0900

    로그인 경로 정리 및 수정3

--- src/main/java/com/film/controller/LoginController.java
+++ src/main/java/com/film/controller/LoginController.java
@@ -48,7 +48,7 @@ public class LoginController {
 		this.naverLoginBO = naverLoginBO; } 
 
 	//로그인 첫 화면 요청 메소드 
-	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST }) 
+	@RequestMapping(value = "/login") 
 	public String login(Model model, HttpSession session) {
 		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */ 
 
@@ -61,7 +61,6 @@ public class LoginController {
 			model.addAttribute("page", "login/login.jsp");
 			return "view";
 		}
-
 	} 
 
 
