24dd7e7d37a400e39d752ae096142971e59653f5
wonhyoung <ybgd18@gmail.com>
2021-10-14 17:17:33 +0900

    무비포스트css수정 더보기 추가

---
+++ src/main/webapp/resources/js/postmovie.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+
+let post_movie = document.getElementsByClassName("grid-item");
+let post_movie4 =3;
+
+for(let i=0; i<post_movie.length; i++)
+{
+	if(i>3)
+	{
+		post_movie[i].style.display="none";
+	}
+}
+let more_movie = document.getElementById("more_post");
+
+function showMore()
+{
+	post_movie4 += 4;
+	for(let i=0; i<=post_movie4; i++)
+	{
+		if(i>3)
+		{
+			post_movie[i].style.display="flex";
+		}
+		if(i===post_movie.length-1){
+			more_movie.remove();
+		}
+	}
+}
