dec5ae6f98808f5457a96ed0327ed7246e557784
yjink <yjin.k12@gmail.com>
2021-10-15 10:59:19 +0900

    007, 짱구 포스트 이미지

--- src/main/java/com/film/controller/AdminController.java
+++ src/main/java/com/film/controller/AdminController.java
@@ -11,14 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.MultipartRequest;
 
 import com.film.dto.NoticePageDTO;
 import com.film.dto.ProductDTO;
