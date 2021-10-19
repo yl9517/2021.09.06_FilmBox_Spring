1f8a4f8bd9eb4e804811d436993ba60b8f226c0a
DESKTOP-F89735T\이여름 <yl9517@naver.com>
2021-10-19 12:00:57 +0900

    [fix] 예매 상태변경 에러 수정

--- src/main/java/com/film/data/ConditionUpdate.java
+++ src/main/java/com/film/data/ConditionUpdate.java
@@ -18,7 +18,7 @@ public class ConditionUpdate {
 	//예매 상태 업데이트
 	
 //	@Scheduled(fixedRate = 10000)
-	@Scheduled(cron = "10 0 10,12,14,18 * * ?") //아무요일 매월 매일 10,12..시 10초마다
+	@Scheduled(cron = "10 0 12,14,16,18 * * ?") //아무요일 매월 매일 10,12..시 10초마다
 	public void updateList() {
 		service.updateCondition();
 	}
